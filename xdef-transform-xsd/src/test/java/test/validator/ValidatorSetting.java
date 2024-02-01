8797a0fc56edb1a3e1cfb32d5a214f6da333e0af
Vladimír Šišma <sisma@syntea.cz>
2024-02-01 15:08:24 +0100

    drobnost: zlepseni kodu/odstraneni java-warnings, oprava preklepu v junit-testech (CKP-1335)

--- xdef-transform-xsd/src/test/java/test/validator/ValidatorSetting.java
+++ xdef-transform-xsd/src/test/java/test/validator/ValidatorSetting.java
@@ -4,11 +4,11 @@ package test.validator;
  * @author smid
  * @since 2021-05-28
  */
-public interface ValidatorSetting {
+public class ValidatorSetting {
 
-    boolean LOG_OUTPUT_ENABLED = false;
-    boolean WRITE_OUTPUT_INTO_FILE = true;
-    boolean VALIDATE_XML_AGAINST_REF_FILE = true;
-    boolean PRINT_XML_VALIDATION_ERRORS = true;
+    public static boolean LOG_OUTPUT_ENABLED = false;
+    public static boolean WRITE_OUTPUT_INTO_FILE = true;
+    public static boolean VALIDATE_XML_AGAINST_REF_FILE = true;
+    public static boolean PRINT_XML_VALIDATION_ERRORS = true;
 
 }
